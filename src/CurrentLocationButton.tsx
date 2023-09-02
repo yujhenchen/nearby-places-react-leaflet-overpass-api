d0e7aa136da23d3df909cd4931f3b9de0ab3d02b
yujhenchen <yujhen.chen93@gmail.com>
2023-09-02 22:06:06 +0800

    add map

--- src/CurrentLocationButton.tsx
+++ src/CurrentLocationButton.tsx
@@ -13,7 +13,7 @@ export default function CurrentLocationButton() {
         <path
           fill="none"
           stroke="#6b7280"
-          stroke-linecap="square"
+          strokeLinecap="square"
           d="M7.5.5v14m7-7.005H.5m13 0a6.006 6.006 0 0 1-6 6.005c-3.313 0-6-2.694-6-6.005a5.999 5.999 0 0 1 6-5.996a6 6 0 0 1 6 5.996Z"
         />
       </svg>
--- src/MapLayout.tsx
+++ src/MapLayout.tsx
@@ -1,12 +1,25 @@
 import React from "react";
 import CurrentLocationButton from "./CurrentLocationButton";
+import { MapContainer, Marker, Popup, TileLayer } from "react-leaflet";
 
 export default function MapLayout() {
   return (
-    <div className="w-screen h-screen bg-gray-200">
+    <div className="w-screen h-screen ">
       <div className="fixed w-10 aspect-square flex items-center place-content-center bottom-64 right-8 lg:bottom-24 lg:right-24">
         <CurrentLocationButton />
       </div>
+
+      <MapContainer center={[51.505, -0.09]} zoom={13} scrollWheelZoom={false}>
+        <TileLayer
+          attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+          url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+        />
+        <Marker position={[51.505, -0.09]}>
+          <Popup>
+            A pretty CSS3 popup. <br /> Easily customizable.
+          </Popup>
+        </Marker>
+      </MapContainer>
     </div>
   );
 }
