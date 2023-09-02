a371bb5814a608c9020d2c534c56aa30016df3b0
yujhenchen <yujhen.chen93@gmail.com>
2023-09-02 20:34:17 +0800

    fix col no wrap does not work

--- src/ResultContainer.tsx
+++ src/ResultContainer.tsx
@@ -3,7 +3,7 @@ import ResultCard from "./ResultCard";
 
 export default function ResultContainer() {
   return (
-    <section className="fixed h-48 bottom-4 left-0 lg:left-4 lg:top-0 lg:bottom-auto lg:w-48 lg:h-screen bg-gray-500 flex lg:flex-col flex-nowrap items-center p-4 space-x-4 lg:space-x-0 lg:space-y-4 overscroll-contain">
+    <section className="fixed h-48 bottom-4 left-0 lg:left-4 lg:top-0 lg:bottom-auto lg:w-48 lg:h-screen bg-gray-500 flex lg:inline-block flex-nowrap items-center p-4 space-x-4 lg:space-x-0 lg:space-y-4">
       {Array.from(Array(10).keys()).map((x) => (
         <ResultCard key={x} />
       ))}
