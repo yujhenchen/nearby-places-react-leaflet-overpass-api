0e4ef49b4a243f7f17aaf439366bb00af290be87
yujhenchen <yujhen.chen93@gmail.com>
2023-09-02 21:16:35 +0800

    add location button

--- src/MapLayout.tsx
+++ src/MapLayout.tsx
@@ -1,5 +1,12 @@
 import React from "react";
+import CurrentLocationButton from "./CurrentLocationButton";
 
 export default function MapLayout() {
-  return <div className="w-full h-screen bg-gray-200">map</div>;
+  return (
+    <div className="w-screen h-screen bg-gray-200">
+      <div className="fixed w-10 aspect-square flex items-center place-content-center bottom-64 right-8 lg:bottom-24 lg:right-24">
+        <CurrentLocationButton />
+      </div>
+    </div>
+  );
 }
