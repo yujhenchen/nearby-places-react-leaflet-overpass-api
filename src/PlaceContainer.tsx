23b2287640eb8d1aaecddfc33e86b40c8ba55def
yujhenchen <yujhen.chen93@gmail.com>
2023-09-03 18:55:04 +0800

    hide scrollbar

--- src/PlaceContainer.tsx
+++ src/PlaceContainer.tsx
@@ -10,7 +10,7 @@ type Props = {
 export default function PlaceContainer({ currentPosition, places }: Props) {
   if (places.length > 0) {
     return (
-      <section className="fixed flex flex-nowrap w-screen h-48 bottom-2 left-0 lg:top-0 lg:bottom-auto lg:w-48 lg:h-screen backdrop-blur lg:inline-block items-center p-4 space-x-4 lg:space-x-0 lg:space-y-4 overflow-x-scroll overflow-y-hidden lg:overflow-x-hidden lg:overflow-y-scroll scrollbar-thin scrollbar-thumb-gray-600">
+      <section className="fixed flex flex-nowrap w-screen h-48 bottom-3 left-0 lg:left-3 lg:top-0 lg:bottom-auto lg:w-48 lg:h-screen backdrop-blur lg:inline-block items-center p-4 space-x-4 lg:space-x-0 lg:space-y-4 overflow-x-scroll overflow-y-hidden lg:overflow-x-hidden lg:overflow-y-scroll scrollbar-none scrollbar-thumb-gray-600">
         {places.map((place) => (
           <PlaceCard
             key={place.id}
