253b5603dc62a8d026143aef93380b214610d521
yujhenchen <yujhen.chen93@gmail.com>
2023-09-04 09:29:09 +0800

    remove not need component

--- src/PlaceCard.tsx
+++ src/PlaceCard.tsx
@@ -1,15 +1,25 @@
 type Props = {
+  isSelected: boolean;
   distance: number;
   name: string;
   opening_hours: string;
 };
 
-export default function PlaceCard({ distance, name, opening_hours }: Props) {
+export default function PlaceCard({
+  isSelected,
+  distance,
+  name,
+  opening_hours,
+}: Props) {
   return (
-    <div className="w-52 h-full aspect-[4/3] rounded-lg flex flex-col space-y-1 bg-gray-100 px-4 py-2 text-xs lg:text-sm lg:w-full lg:h-40 hover:shadow-lg duration-300 lg:overflow-y-hidden hover:overflow-y-auto scrollbar-thin scrollbar-thumb-gray-300 hover:duration-300">
-      <span>
-        <b>Name:</b> {name}
-      </span>
+    <div
+      className={
+        isSelected
+          ? "w-52 h-full aspect-[4/3] rounded-lg flex flex-col space-y-1 scale-110 bg-gray-100 px-4 py-2 text-xs lg:text-sm lg:w-full lg:h-40 shadow-lg lg:overflow-y-hidden hover:overflow-y-auto scrollbar-thin scrollbar-thumb-gray-300 hover:duration-300"
+          : "w-52 h-full aspect-[4/3] rounded-lg flex flex-col space-y-1 bg-gray-100 px-4 py-2 text-xs lg:text-sm lg:w-full lg:h-40 hover:shadow-lg duration-300 lg:overflow-y-hidden hover:overflow-y-auto scrollbar-thin scrollbar-thumb-gray-300 hover:duration-300"
+      }
+    >
+      <span>{name}</span>
       <span>
         <b>Distance:</b> {distance} km
       </span>
--- src/PlaceContainer.tsx
+++ src/PlaceContainer.tsx
@@ -1,5 +1,4 @@
 import PlaceCard from "./PlaceCard";
-import PlaceCardSelected from "./PlaceCardSelected";
 import { GeoPosition, PlaceNode } from "./libs/types";
 import { distance } from "./libs/utils";
 
@@ -17,35 +16,24 @@ export default function PlaceContainer({
   if (places.length > 0) {
     return (
       <section className="fixed flex flex-nowrap w-screen h-48 bottom-3 left-0 lg:left-3 lg:top-0 lg:bottom-auto lg:w-56 lg:h-screen backdrop-blur lg:inline-block items-center p-4 space-x-4 lg:space-x-0 lg:space-y-4 overflow-x-scroll overflow-y-hidden lg:overflow-x-hidden lg:overflow-y-scroll scrollbar-none scrollbar-thumb-gray-600">
-        {places.map((place) =>
-          selectedPosition !== null &&
-          selectedPosition.lat === place.lat &&
-          selectedPosition.lon === place.lon ? (
-            <PlaceCardSelected
-              key={place.id}
-              distance={distance(
-                currentPosition.lat,
-                currentPosition.lon,
-                place.lat,
-                place.lon
-              )}
-              name={place.tags.name}
-              opening_hours={place.tags.opening_hours}
-            />
-          ) : (
-            <PlaceCard
-              key={place.id}
-              distance={distance(
-                currentPosition.lat,
-                currentPosition.lon,
-                place.lat,
-                place.lon
-              )}
-              name={place.tags.name}
-              opening_hours={place.tags.opening_hours}
-            />
-          )
-        )}
+        {places.map((place) => (
+          <PlaceCard
+            key={place.id}
+            isSelected={
+              selectedPosition !== null &&
+              selectedPosition.lat === place.lat &&
+              selectedPosition.lon === place.lon
+            }
+            distance={distance(
+              currentPosition.lat,
+              currentPosition.lon,
+              place.lat,
+              place.lon
+            )}
+            name={place.tags.name}
+            opening_hours={place.tags.opening_hours}
+          />
+        ))}
       </section>
     );
   } else {
